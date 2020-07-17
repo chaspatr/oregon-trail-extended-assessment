e2e58bb326f2931582f363752233a9486596be4d
Chase Patrick <chasepatrick117@gmail.com>
2020-07-17 12:51:05 -0400

    completed the 2.0 assessment

--- assets/scripts/classes.js
+++ assets/scripts/classes.js
@@ -45,3 +45,36 @@ class Wagon {
     return food.reduce((current, next) => current + next);
   }
 }
+
+class Doctor extends Traveler {
+  constructor(name) {
+    super(name);
+  }
+  heal(traveler) {
+    if (traveler.isHealthy === false) traveler.isHealthy = true;
+  }
+}
+
+class Hunter extends Traveler {
+  constructor(name) {
+    super(name);
+    this.food = 2;
+  }
+  hunt() {
+    this.food += 5;
+  }
+  eat() {
+    if (this.food >= 2) {
+      this.food -= 2;
+    } else {
+      this.food = 0;
+      this.isHealthy = false;
+    }
+  }
+  giveFood(traveler, numOfFoodUnits) {
+    if (this.food >= numOfFoodUnits) {
+      this.food -= numOfFoodUnits;
+      traveler.food += numOfFoodUnits;
+    }
+  }
+}
